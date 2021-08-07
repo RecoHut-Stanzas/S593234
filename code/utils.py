080cf1cd4c8335686c362bff00a0ce2a58cf98fc
reco-tut <recotut@recohut.com>
2021-08-07 19:48:29 +0000

    commit

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,4 @@
-def prefilter_items(data, take_n_popular=None, exclude_popular=None, filter_weeks_no_sold=None):
+def prefilter_items(data, take_n_popular=5000, item_features=None, exclude_popular=None, filter_weeks_no_sold=None):
     # Let's remove the most popular products (they will be bought anyway)
     if exclude_popular is not None:
         popularity = data.groupby('item_id')['quantity'].sum().reset_index()
@@ -18,12 +18,32 @@ def prefilter_items(data, take_n_popular=None, exclude_popular=None, filter_week
         data.loc[data['week_no'] > filter_weeks_no_sold, 'item_id'] = 999999
 
     # Let's remove the categories that are not interesting for recommendations (department)
+    if item_features is not None:
+        department_size = pd.DataFrame(item_features. \
+                                       groupby('department')['item_id'].nunique(). \
+                                       sort_values(ascending=False)).reset_index()
+
+        department_size.columns = ['department', 'n_items']
+        rare_departments = department_size[department_size['n_items'] < 150].department.tolist()
+        items_in_rare_departments = item_features[
+            item_features['department'].isin(rare_departments)].item_id.unique().tolist()
+
+        data = data[~data['item_id'].isin(items_in_rare_departments)]
 
     # We will remove goods that are too cheap (we will not make money on them). 1 purchase from mailing lists costs 60 rubles.
+    data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
+    data = data[data['price'] > 2]
 
     # Let's remove too expensive goods
+    data = data[data['price'] < 50]
 
-    # Other filters
+    # Let's take the top in popularity
+    popularity = data.groupby('item_id')['quantity'].sum().reset_index()
+    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
+    top = popularity.sort_values('n_sold', ascending=False).head(take_n_popular).item_id.tolist()
+
+    # Let's create a fictitious item_id (if the user bought goods from the top 5000, then he "bought" such a product)
+    data.loc[~data['item_id'].isin(top), 'item_id'] = 999999
 
     return data
 
